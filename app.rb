e6284deef0bd08cffc066c8948b46c6575bb8621
Larry Wright <larrywright@gmail.com>
2012-02-20 14:42:43 -0600

    set :public is deprecated

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ Dir["models/*.rb"].each do |model|
 end
 
 class App < Sinatra::Base
-  set :public, File.dirname(__FILE__) + '/public'
+  set :public_folder, File.dirname(__FILE__) + '/public'
   require "sinatra/reloader"
 
   get '/' do
